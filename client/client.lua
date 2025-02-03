e7a0c698d183e8a4a42325cbf0050ccf547bfc4b
YuvalWeb <75447942+YuvalWeb8@users.noreply.github.com>
2025-02-03 20:01:20 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -26,7 +26,9 @@ function updateZones()
                     name = 'spawn_menu',
                     label = 'Open teleport menu',
                     onSelect = function()
-                        local organization = exports.core_gangs:getPlayerOrganization()
+                        if Config.TeleportGang thne
+                            local organization = exports.core_gangs:getPlayerOrganization()
+                        end
 
                         local options = {
                             {
@@ -49,29 +51,31 @@ function updateZones()
                             }
                         }
 
-                        if organization and Config.GangTeleport then
-                            table.insert(options, {
-                                title = ('Gang: %s'):format(organization),
-                                description = 'Teleport to your gang neighborhood',
-                                icon = 'person-rifle',
-                                iconColor = 'green',
-                                onSelect = function()
-                                    local gangLocations = {
-                                        CHANG = {coords = {x = -684.38, y = -889.07, z = 24.50}, message = 'You are teleporting to Chang neighborhood'},
-                                        GSF = {coords = {x = -136.11, y = -1594.91, z = 34.24}, message = 'You are teleporting to GSF neighborhood'},
-                                        BALLAS = {coords = {x = 105.71, y = -1940.45, z = 20.80}, message = 'You are teleporting to Ballas neighborhood'}
-                                    }
-
-
-                                    local gangData = gangLocations[organization]
-
-                                    if gangData then
-                                        teleportPlayer(gangData.coords, gangData.message)
-                                    else
-                                        TriggerEvent('codem-notification:Create', 'Your gang doesn\'t have a neighborhood.', 'error', 'Green - Gangs & DM', 5000)
+                        if Config.GangTeleport then
+                                if organization then
+                                table.insert(options, {
+                                    title = ('Gang: %s'):format(organization),
+                                    description = 'Teleport to your gang neighborhood',
+                                    icon = 'person-rifle',
+                                    iconColor = 'green',
+                                    onSelect = function()
+                                        local gangLocations = {
+                                            CHANG = {coords = {x = -684.38, y = -889.07, z = 24.50}, message = 'You are teleporting to Chang neighborhood'},
+                                            GSF = {coords = {x = -136.11, y = -1594.91, z = 34.24}, message = 'You are teleporting to GSF neighborhood'},
+                                            BALLAS = {coords = {x = 105.71, y = -1940.45, z = 20.80}, message = 'You are teleporting to Ballas neighborhood'}
+                                        }
+    
+    
+                                        local gangData = gangLocations[organization]
+    
+                                        if gangData then
+                                            teleportPlayer(gangData.coords, gangData.message)
+                                        else
+                                            TriggerEvent('codem-notification:Create', 'Your gang doesn\'t have a neighborhood.', 'error', 'Green - Gangs & DM', 5000)
+                                        end
                                     end
-                                end
-                            })
+                                })
+                            end
                         end
 
                         lib.registerContext({
